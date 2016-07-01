aff8042711a86b7efa70e31a63b75f7a5021eb5a
kwabratseur <jeroenende@hotmail.com>
2016-07-01 18:24:16 +0200

    bug
    - fixed bug with placing Flag2 and infinite healer spawning
    - fixed bug which occurs when no creeps are present in farm room

--- Test 2/farm.tile.js
+++ Test 2/farm.tile.js
@@ -32,7 +32,7 @@ var farmTile = {
         //console.log(TargetRoom.room.controller.reservation.ticksToEnd);
         //console.log();
         
-        if(TargetRoom.room.controller.reservation.username == 'Kwabratseur'){
+        if(TargetRoom != undefined && TargetRoom.room.controller.reservation.username == 'Kwabratseur'){
             
             Tno = 4;
         }
