a7d09c3eb3c6da3f89e98328a6501885f78c5a0e
kwabratseur <jeroenende@hotmail.com>
2016-07-03 13:48:45 +0200

    fixed bug
    incorrect define check.

--- Test 2/role.army.js
+++ Test 2/role.army.js
@@ -5,10 +5,10 @@ var roleArmy = {
         if(creep.memory.HarvestTile != creep.room.name){
 	            creep.moveTo(Game.flags[creep.memory.FlagName]);
 	    }else{
-	        if(creep.pos.inRangeTo(Game.flags[creep.memory.FlagName],10)){
+	        if(creep.pos.inRangeTo(Game.flags[creep.memory.FlagName],25)){
                 var hostiles = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
                 var hostileBuildings = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
-                if(hostiles.length > 0){
+                if(hostiles != undefined){
                     if(creep.attack(hostiles) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(hostiles, {reusePath: 50});
                     }
